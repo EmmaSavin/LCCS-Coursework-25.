d4c5ee55b74a1b9349b6119e85ccfa12d6e84794
EmmaSavin <154214967+EmmaSavin@users.noreply.github.com>
2025-03-04 09:22:10 +0000

    Add files via upload

--- flask_main.py
+++ flask_main.py
@@ -15,6 +15,36 @@ def home():
     #pass these onto the webpage (with a chance to rename them)
     return render_template("index.html", cwinfo=cwinfo)
 
+#Route for second page
+@app.route('/second-page')
+def second_page():
+    return render_template("second-page.html")
+
+#Route for third page
+@app.route('/third-page')
+def third_page():
+    return render_template("third-page.html")
+
+#Route for fourth page
+@app.route('/fourth-page')
+def fourth_page():
+    return render_template("fourth-page.html")
+
+#Route for fifth page
+@app.route('/fifth-page')
+def fifth_page():
+    return render_template("fifth-page.html")
+
+#Route for sixth page
+@app.route('/sixth-page')
+def sixth_page():
+    return render_template("sixth-page.html")
+
+#Route for seventh page
+@app.route('/seventh-page')
+def seventh_page():
+    return render_template("seventh-page.html")
+
 # Run the app (this starts the server)
-if __name__ == "__main__":
-    app.run(debug=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5022, debug=False)

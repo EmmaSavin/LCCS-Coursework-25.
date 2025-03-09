3ffd7e4527bd5ab3f02987cd5eac65d9131eeb1a
EmmaSavin <154214967+EmmaSavin@users.noreply.github.com>
2025-03-09 19:55:05 +0000

    Delete BR3.py

--- BR3.py
+++
@@ -1,8 +0,0 @@
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("index.html",
-                           scatter_plot_html=scatter_plot_html,
-                           temp_barchart_html=temp_barchart_html,
-                           salinity_barchart_html=salinity_barchart_html)
